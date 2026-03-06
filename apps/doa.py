66aed2d7609f2a053e8ce424a8180072bece11a7
Till Bey <till.bey@surf.nl>
2026-03-06 10:00:39 +0100

    improve data loading, make charts interactive

--- apps/doa.py
+++ apps/doa.py
@@ -1,5 +1,5 @@
 # /// script
-# requires-python = ">=3.13"
+# requires-python = ">=3.12"
 # dependencies = [
 #     "altair==6.0.0",
 #     "fastexcel==0.19.0",
@@ -14,6 +14,17 @@ import marimo
 
 __generated_with = "0.19.4"
 app = marimo.App(width="medium")
+
+
+@app.cell
+async def _():
+    import micropip
+    await micropip.install(['polars', 'altair', 'openpyxl'])
+
+    import marimo as mo
+    import polars as pl
+    import altair as alt
+    return alt, mo, pl
 
 
 @app.cell
@@ -34,6 +45,12 @@ def _(domain_chart, mo, platform_chart, publisher_chart, years_chart):
 
 
 @app.cell
+def _(journals, mo):
+    get_state, set_state = mo.state(journals)
+    return get_state, set_state
+
+
+@app.cell
 def _(mo):
     mo.md(r"""
     The data behind the dashboard can be found below, and also on Zenodo under [10.5281/zenodo.17185088](https://doi.org/10.5281/zenodo.17185088).
@@ -42,16 +59,28 @@ def _(mo):
 
 
 @app.cell
-def _(data_path, pl):
-    journals = pl.read_excel(data_path, sheet_name='Included Diamond OA Journals', engine='openpyxl')
+def _(mo):
+    data_selector = mo.ui.dropdown({
+        'data from Google Sheet': ('https://docs.google.com/spreadsheets/d/1CaHEgS6Nu9OAeyEHe9tmCpMgnhRr196zFp2gsRw20YM/export?format=xlsx', 'Included Diamond OA Journals'),
+        'data from Zenodo': ('https://zenodo.org/api/records/17195184/files/Diamond%20OA%20-%20NL%20Journals%20-%20WGNM%20-%20FINAL%20-%20SHAREABLE-version%202.xlsx/content', 'Included DOA Journals')
+    }, value='data from Google Sheet')
+    data_selector
+    return (data_selector,)
+
+
+@app.cell
+def _(data_selector, pl):
+    data_path, sheet_name = data_selector.value
+
+    journals = pl.read_excel(data_path, sheet_name=sheet_name, engine='openpyxl').fill_null('unknown')
     journals
     return (journals,)
 
 
 @app.cell
-def _(alt, journals):
-    years_chart = (
-        alt.Chart(journals)
+def _(alt, get_state, journals, mo, set_state):
+    years_chart = mo.ui.altair_chart(
+        alt.Chart(journals if len(get_state()) == 0 else get_state())
         .mark_bar()
         .encode(
             x=alt.X(field='DOAJ - Year OA', type='quantitative'),
@@ -63,82 +92,68 @@ def _(alt, journals):
                 alt.Tooltip(field='Model')
             ]
         )
-        .properties(height=300, width=300)
+        .properties(height=300, width=300),
+        on_change=set_state
     )
     return (years_chart,)
 
 
 @app.cell
-async def _():
-    # data_path = mo.notebook_location() / 'public' / 'Diamond OA - NL Journals - WGNM - FINAL - SHAREABLE-3.xlsx'
-
-    import micropip
-    await micropip.install('polars')
-    await micropip.install('altair')
-    await micropip.install('openpyxl')
-
-    data_path = 'https://docs.google.com/spreadsheets/d/1CaHEgS6Nu9OAeyEHe9tmCpMgnhRr196zFp2gsRw20YM/export?format=xlsx'
-    return (data_path,)
-
-
-@app.cell
-def _():
-    import marimo as mo
-    import polars as pl
-    import altair as alt
-    return alt, mo, pl
-
-
-@app.cell
-def _(alt, journals):
-    domain_chart = (
-        alt.Chart(journals)
+def _(alt, get_state, journals, mo, set_state):
+    domain_chart = mo.ui.altair_chart(
+        alt.Chart(journals if len(get_state()) == 0 else get_state())
         .mark_arc(innerRadius=80)
         .encode(
             color=alt.Color(field='OpenAlex - domain', type='nominal'),
             theta=alt.Theta(aggregate='count', type='quantitative'),
             tooltip=[
                 alt.Tooltip(aggregate='count'),
+                alt.Tooltip(field='OpenAlex - domain'),
                 alt.Tooltip(field='Journal Title')
             ]
         )
-        .properties(height=300, width=300)
+        .properties(height=300, width=300),
+        on_change=set_state
     )
     return (domain_chart,)
 
 
 @app.cell
-def _(alt, journals):
-    publisher_chart = (
-        alt.Chart(journals)
+def _(alt, get_state, journals, mo, set_state):
+    publisher_chart = mo.ui.altair_chart(
+        alt.Chart(journals if len(get_state()) == 0 else get_state())
         .mark_arc(innerRadius=80)
         .encode(
             color=alt.Color(field='Publisher', type='nominal'),
             theta=alt.Theta(aggregate='count', type='quantitative'),
             tooltip=[
                 alt.Tooltip(aggregate='count'),
+                alt.Tooltip(field='Publisher'),
                 alt.Tooltip(field='Journal Title')
             ]
         )
-        .properties(height=300, width=300)
+        .properties(height=300, width=300),
+        on_change=set_state
     )
     return (publisher_chart,)
 
 
 @app.cell
-def _(alt, journals):
-    platform_chart = (
-        alt.Chart(journals)
+def _(alt, get_state, journals, mo, set_state):
+    platform_chart = mo.ui.altair_chart(
+        alt.Chart(journals if len(get_state()) == 0 else get_state())
         .mark_arc(innerRadius=80)
         .encode(
             color=alt.Color(field='Technical platform', type='nominal'),
             theta=alt.Theta(aggregate='count', type='quantitative'),
             tooltip=[
                 alt.Tooltip(aggregate='count'),
+                alt.Tooltip(field='Technical platform'),
                 alt.Tooltip(field='Publisher')
             ]
         )
-        .properties(height=300, width=300)
+        .properties(height=300, width=300),
+        on_change=set_state
     )
     return (platform_chart,)
 
